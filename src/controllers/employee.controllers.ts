0357824a954a626b07f4967ac7f67df4c7c7a977
Dize dev <axiozweb2022@gmail.com>
2025-07-03 21:18:48 -0500

    feat: mejorar la gestión de errores y respuestas en los controladores de empleados

--- src/controllers/employee.controllers.ts
+++ src/controllers/employee.controllers.ts
@@ -3,63 +3,89 @@ import { pool } from '../db';
 import type { ResultSetHeader, RowDataPacket } from 'mysql2';
 
 export const getEmployees = async (req: Request, res: Response) => {
-	const rows = await pool.query<RowDataPacket[]>('SELECT * FROM employee');
-	res.status(200).json({ sucsses: true, message: rows.at(0) });
+	try {
+		const rows = await pool.query<RowDataPacket[]>('SELECT * FROM employee');
+		res.status(200).json({ sucsses: true, message: rows.at(0) });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ success: false, message: 'Internal Server Error' });
+	}
 };
 
 export const getEmployee = async (req: Request, res: Response) => {
 	const { id } = req.params;
-	const [rows] = await pool.query<RowDataPacket[]>(
-		'SELECT id, name, salary FROM employee WHERE id = ? ',
-		[id]
-	);
+	try {
+		const [rows] = await pool.query<RowDataPacket[]>(
+			'SELECT id, name, salary FROM employee WHERE id = ? ',
+			[id]
+		);
 
-	if (rows.length <= 0)
-		return res.status(404).json({ sucsses: false, message: 'Employee not found' });
+		if (rows.length <= 0)
+			return res.status(404).json({ sucsses: false, message: 'Employee not found' });
 
-	res.status(200).json({ sucsses: true, message: rows.at(0) });
+		res.status(200).json({ sucsses: true, message: rows.at(0) });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ sucsses: false, message: 'Internal Server Error' });
+	}
 };
 
 export const createEmployee = async (req: Request, res: Response) => {
 	const { name, salary } = req.body;
-	const [rows] = await pool.query<ResultSetHeader>(
-		'INSERT INTO employee (name, salary) VALUES (?, ?)',
-		[name, salary]
-	);
+	try {
+		const [rows] = await pool.query<ResultSetHeader>(
+			'INSERT INTO employee (name, salary) VALUES (?, ?)',
+			[name, salary]
+		);
 
-	res.status(200).json({ sucsses: true, message: { id: rows?.insertId, name, salary } });
+		res
+			.status(200)
+			.json({ sucsses: true, message: { id: rows?.insertId, name, salary } });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ sucsses: false, message: 'Internal server Error' });
+	}
 };
 
 export const updateEmploye = async (req: Request, res: Response) => {
 	const { id } = req.params;
 	const { name, salary } = req.body;
+	try {
+		const [rows] = await pool.query<ResultSetHeader>(
+			'UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?',
+			[name, salary, id]
+		);
 
-	const [rows] = await pool.query<ResultSetHeader>(
-		'UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?',
-		[name, salary, id]
-	);
+		if (rows.affectedRows <= 0)
+			return res.status(404).json({ success: false, message: 'Employee not found' });
+		// ? BUSCAR EL EMPLEADO ACTUALIZADO MEDIANTE EL ID DEL EMPLEADO
+		const [result] = await pool.query<RowDataPacket[]>(
+			'SELECT id, name, salary  FROM  employee WHERE id = ?',
+			[id]
+		);
 
-	if (rows.affectedRows <= 0)
-		return res.status(404).json({ success: false, message: 'Employee not found' });
-	// ? BUSCAR EL EMPLEADO ACTUALIZADO MEDIANTE EL ID DEL EMPLEADO
-	const [result] = await pool.query<RowDataPacket[]>(
-		'SELECT id, name, salary  FROM  employee WHERE id = ?',
-		[id]
-	);
-	console.log(result);
-
-	res.status(200).json({ success: true, message: result.at(0) });
+		res.status(200).json({ success: true, message: result.at(0) });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ success: false, message: 'Internal Server Error' });
+	}
 };
 
 export const deleteEmployee = async (req: Request, res: Response) => {
 	const { id } = req.params;
-	// ? consulta para actualizar un empleado
-	const [rows] = await pool.query<ResultSetHeader>('DELETE FROM employee WHERE id = ?', [
-		id,
-	]);
+	try {
+		// ? consulta para actualizar un empleado
+		const [rows] = await pool.query<ResultSetHeader>(
+			'DELETE FROM employee WHERE id = ?',
+			[id]
+		);
 
-	if (rows.affectedRows <= 0)
-		res.status(404).json({ success: false, message: 'Employee not found' });
+		if (rows.affectedRows <= 0)
+			res.status(404).json({ success: false, message: 'Employee not found' });
 
-	res.status(200).json({ success: true, message: 'delete employee' });
+		res.status(200).json({ success: true, message: 'delete employee' });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ success: false, message: 'Internal Server Error' });
+	}
 };
